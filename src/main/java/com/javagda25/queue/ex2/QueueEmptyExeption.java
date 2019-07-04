fb279cb1c89f531563405962366b6eb6a7e0fd61
Agata <agata.weltrowska@outlook.com>
2019-07-04 18:46:11 +0200

    comit

--- src/main/java/com/javagda25/queue/ex2/QueueEmptyExeption.java
+++ src/main/java/com/javagda25/queue/ex2/QueueEmptyExeption.java
@@ -1,6 +1,6 @@
 package com.javagda25.queue.ex2;
 
-public class QueueEmptyExeption extends Throwable {
+public class QueueEmptyExeption extends RuntimeException {
     public QueueEmptyExeption(String s) {
         super(s);
     }
